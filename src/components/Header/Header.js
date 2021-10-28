397d8923d99dc42e3caf5a9e4a5e2635363fd2e5
IgorLazar94 <81099964+IgorLazar94@users.noreply.github.com>
2021-10-28 17:08:13 +0300

    Header [end links]

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,19 +3,22 @@ const Header = (props) => {
   return (
     <div className="header">
       <div className="header__logo">
-        <div className="header__logo-icon">
-          {/* Добавить иконку */}
-          {/* <img    src="./components/icons/joystick.svg" alt="" /> */}
-        </div>
-        <p className="header__logo-title">Vlados</p>
-        <p className="header__logo-postTitle">games</p>
+        <a href="/">
+          <div className="header__logo-icon"></div>
+          <p className="header__logo-title">Vlados</p>
+          <p className="header__logo-postTitle">games</p>
+        </a>
       </div>
       <div className="header__menu">
         <div className="header__menu-games">
           <a href="/">Games</a>
         </div>
-        <div className="header__menu-aboutUs">About us</div>
-        <div className="header__menu-career">Career</div>
+        <div className="header__menu-aboutUs">
+          <a href="/">About Us</a>
+        </div>
+        <div className="header__menu-career">
+          <a href="/">Career</a>
+        </div>
       </div>
     </div>
   );
