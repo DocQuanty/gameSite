755030a64c78bb3a913e54fa2d84bcae358607d5
IgorLazar94 <81099964+IgorLazar94@users.noreply.github.com>
2021-10-29 15:24:47 +0300

    header [add media]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.scss";
-import Header from "./components/Header/Header";
 import SectionHero from "./pages/sectionHero/sectionHero";
 
 const App = (props) => {
   return (
     <div className="App">
-      {/* <Header></Header> */}
       <SectionHero />
     </div>
   );
