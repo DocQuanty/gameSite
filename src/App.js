05700c672a7732919643ee1d56d988c39b9503b9
DocQuanty <Urchikak8@gmail.com>
2021-11-02 18:49:49 +0200

    try

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.scss";
+import MainTitle from "./components/MainTitle/MainTitle";
 import SectionHero from "./pages/sectionHero/sectionHero";
 
 const App = (props) => {
   return (
     <div className="App">
+      <MainTitle
+        firstWord={"BRINGING"}
+        mark={"awd"}
+        secondWord={<h1>awdawd</h1>}
+      >
+        hello im a title
+      </MainTitle>
       <SectionHero />
     </div>
   );
