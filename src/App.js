d4a96d261e9877c023ab91a45a1fee59ebc271ab
IgorLazar94 <81099964+IgorLazar94@users.noreply.github.com>
2021-11-03 20:17:57 +0200

    commit [^^^]

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import "./App.scss";
-<<<<<<< HEAD
-// import Header from "./components/Header/Header";
-// import SectionHero from "./pages/sectionHero/sectionHero";
-import SectionGames from "./pages/sectionGames/sectionGames";
-const App = (props) => {
-  return (
-    <div className="App">
-      {/* <SectionHero /> */}
-      <SectionGames></SectionGames>
-=======
+import sectionGames from "./pages/sectionGames/sectionGames";
 import MainTitle from "./components/MainTitle/MainTitle";
 import SectionHero from "./pages/sectionHero/sectionHero";
 
 const App = (props) => {
   return (
     <div className="App">
-      <SectionHero />
->>>>>>> MainTitleComponent
+      <sectionGames />
     </div>
   );
 };
