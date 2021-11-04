a1d2c73a85c7916d583a3f1f1bf9545f701c9e41
IgorLazar94 <81099964+IgorLazar94@users.noreply.github.com>
2021-11-04 18:14:20 +0200

    card [edit html]

--- src/components/CardComponent/CardComponent.js
+++ src/components/CardComponent/CardComponent.js
@@ -1,16 +1,19 @@
 import "./CardComponent.scss";
+import Bulletimage from "../../Icons/Bullet.jpg";
 const Card = (props) => {
   return (
     <div className="card-component__main">
       <div className="card-component__image">
         <a href="/">
-          <img src="./images./bullet.jpg" alt="изображение не отображено" />
+          <img src={Bulletimage} alt="изображение не отображено" />
         </a>
       </div>
-      <div className="card-component__text"></div>
-      {/* <ButtonComponent /> */}
+      <div className="card-component__text">Bullet Collect 3D</div>
+      <div className="card-component__button">{/* <ButtonComponent /> */}</div>
     </div>
   );
 };
 // Последняя строчка так пишется?!
+//Вставка кнопки, если её ещё нет
+//Вставка картинки
 export default Card;
