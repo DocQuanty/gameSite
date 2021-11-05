bbae160d7eec738d5516098ddbdd2d57708a7e49
IgorLazar94 <81099964+IgorLazar94@users.noreply.github.com>
2021-11-05 14:09:52 +0200

    sectionAbout [connect MainTitle and SubTitle]

--- src/pages/SectionAbout/SectionAbout.js
+++ src/pages/SectionAbout/SectionAbout.js
@@ -1,6 +1,6 @@
 import "../SectionAbout/SectionAbout.scss";
-// import Maintitle from "../../components/MainTitle/MainTitle";
-// import SubTitle from "../../components/SubTitle/SubTitle";
+import Maintitle from "../../components/MainTitle/MainTitle";
+import SubTitle from "../../components/SubTitle/SubTitle";
 const sectionAbout = (props) => {
   return <div className="section-about"></div>;
 };
