3035f41577d995e1d2ba3728b04642f2bed1fe8a
DocQuanty <Urchikak8@gmail.com>
2021-11-06 14:49:05 +0200

    fix weight from 170px / 155px

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.scss";
 // import SectionHero from "./pages/sectionHero/sectionHero";
-
+import CardComponent from "./components/CardComponent/CardComponent";
 const App = (props) => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <CardComponent></CardComponent>
+    </div>
+  );
 };
 export default App;
