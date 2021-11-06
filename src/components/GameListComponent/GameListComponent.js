54a5833d96ae05029c4e534d6fa33e3a8d71b611
DocQuanty <Urchikak8@gmail.com>
2021-11-06 18:04:18 +0200

    create link for app store

--- src/components/GameListComponent/GameListComponent.js
+++ src/components/GameListComponent/GameListComponent.js
@@ -10,30 +10,35 @@ class GameListComponent extends React.Component {
         img: "https://media.contentapi.ea.com/content/dam/gin/images/2021/06/battlefield-2042-key-art.jpg.adapt.crop1x1.767p.jpg",
         describe: "Bullet Collect 3D",
         btnInfo: "App Store",
+        link: "https://www.apple.com/ua/app-store/",
       },
       {
         id: 2,
         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsCxuUXimum38SXh7lO3eVjXbrbxwzJ2TUvzBvKo4jGHX2zTS4hngmy3DFMTrkdshlevk&usqp=CAU",
         describe: "Steal N Run: Master Thief",
         btnInfo: "App Store",
+        link: "https://www.apple.com/ua/app-store/",
       },
       {
         id: 3,
         img: "https://media.contentapi.ea.com/content/dam/gin/images/2021/06/battlefield-2042-key-art.jpg.adapt.crop1x1.767p.jpg",
         describe: "Bullet Collect 3D",
         btnInfo: "App Store",
+        link: "https://www.apple.com/ua/app-store/",
       },
       {
         id: 4,
         img: "https://media.contentapi.ea.com/content/dam/gin/images/2021/06/battlefield-2042-key-art.jpg.adapt.crop1x1.767p.jpg",
         describe: "Pack it up! Jigsaw Puzzle Game",
         btnInfo: "App Store",
+        link: "https://www.apple.com/ua/app-store/",
       },
       {
         id: 5,
         img: "https://media.contentapi.ea.com/content/dam/gin/images/2021/06/battlefield-2042-key-art.jpg.adapt.crop1x1.767p.jpg",
         describe: "Coloring Guys io: Run and Paint",
         btnInfo: "App Store",
+        link: "https://www.apple.com/ua/app-store/",
       },
     ],
   };
@@ -45,16 +50,12 @@ class GameListComponent extends React.Component {
           img={el.img}
           describe={el.describe}
           btnInfo={el.btnInfo}
+          link={el.link}
         />
       );
     });
 
-    return (
-      <div className="list-game">
-        {CardItem}
-        {/* <CardComponent /> */}
-      </div>
-    );
+    return <div className="list-game">{CardItem}</div>;
   }
 }
 export default GameListComponent;
