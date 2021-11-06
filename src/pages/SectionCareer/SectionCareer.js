ee0831cbe868ce72308a7a5d180bdea4516261c1
IgorLazar94 <81099964+IgorLazar94@users.noreply.github.com>
2021-11-06 19:16:04 +0200

    SectionCareer [add some styles]

--- src/pages/SectionCareer/SectionCareer.js
+++ src/pages/SectionCareer/SectionCareer.js
@@ -1,23 +1,30 @@
 import "../SectionCareer/SectionCareer.scss";
 import Maintitle from "../../components/MainTitle/MainTitle";
 import SubTitle from "../../components/SubTitle/SubTitle";
+// import ArrowRight from "./icons/arrow-right.svg";
 const SectionCareer = (props) => {
   return (
     <div className="section-career">
       <div className="section-career__wrapper">
         <div className="section-career__container">
-          <div className="section-career__title"></div>
-          <div className="section-career__subtitle"></div>
+          <div className="section-career__maintitle">
+            <Maintitle>
+              <span className="mark">CAREER </span>AT DORFER GAMES
+            </Maintitle>
+          </div>
+          <div className="section-career__subtitle">REMOTE</div>
           <div className="section-career__generalist">
-            <div className="generalist__text"></div>
-            <div className="generalist__develop"></div>
+            <div className="section-career__generalist_text">3D Generalist</div>
+            <div className="section-career__generalist_develop">Develop</div>
           </div>
-          <div className="section-career__generalist_line"></div>
+          {/* <div className="section-career__generalist_line"></div> */}
           <div className="section-career__middle">
-            <div className="middle__text"></div>
-            <div className="middle__develop"></div>
+            <div className="section-career__middle_text">
+              Middle Unity 3D Developer
+            </div>
+            <div className="section-career__middle_develop">Develop</div>
           </div>
-          <div className="section-career__middle_line"></div>
+          {/* <div className="section-career__middle_line"></div> */}
         </div>
       </div>
     </div>
