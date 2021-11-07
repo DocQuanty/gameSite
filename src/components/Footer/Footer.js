bd330eb10c18e611e0733c500fe74feeafd80a9b
IgorLazar94 <81099964+IgorLazar94@users.noreply.github.com>
2021-11-07 14:55:58 +0200

    Footer [create some styles]

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,46 @@
 import "./Footer.scss";
 const Footer = (props) => {
-  return <></>;
+  return (
+    <>
+      <div className="footer__wraper">
+        <div className="footer__background">
+          <div className="footer__container-flex">
+            <div className="container-flex__logo">
+              <a href="/">
+                <p>Vlados</p>
+                <p>Games</p>
+              </a>
+            </div>
+            <div className="container-flex__title">
+              <p>Dorfer Games OÜ, Poznyaki, Kyiv, 02068, Ukraine</p>
+              <p>Reg. No.: 14854266</p>
+              <p>VAT Number: EE102288380</p>
+            </div>
+            <div className="container-flex__icons">
+              <div className="icons__apple"></div>
+              <div className="icons__information"></div>
+              <div className="icons__facebook"></div>
+            </div>
+          </div>
+          <div className="footer__container-subtext-flex">
+            <div className="container-subtext-flex__year">
+              © Dorfer Games, 2021
+            </div>
+            <div className="container-subtext-flex__links">
+              <p>
+                <a href="/">Contact Us</a>
+              </p>
+              <p>
+                <a href="/">Privacy Policy</a>
+              </p>
+              <p>
+                <a href="/">Terms</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 export default Footer;
