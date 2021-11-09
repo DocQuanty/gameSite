348305cd99731482165a1cdbbb6c7ae6568fe085
DocQuanty <Urchikak8@gmail.com>
2021-11-09 15:15:47 +0200

    first init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import "./App.scss";
-import SectionHero from "./pages/sectionHero/sectionHero";
 
 const App = (props) => {
-  return (
-    <div className="App">
-      <SectionHero />
-    </div>
-  );
+  return <div className="App"></div>;
 };
 export default App;
