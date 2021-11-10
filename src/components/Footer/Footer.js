8ce45ea8af2386fdc9718f2961df149b7580e165
IgorLazar94 <81099964+IgorLazar94@users.noreply.github.com>
2021-11-10 15:59:21 +0200

    Footer [svg ]

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import "./Footer.scss";
 import ButtonComponent from "../ButtonComponent/ButtonComponent";
-import Information from "./Icons/Information.svg";
+// import Information from "./Icons/Information.svg";
+import Facebook from "./Icons/facebook.svg";
 const Footer = (props) => {
   return (
     <>
@@ -29,9 +30,19 @@ const Footer = (props) => {
                 </ButtonComponent> */}
               </div>
               <div className="icons__information">
-                <img src={Information} alt="" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="#fff"
+                  width="50"
+                  height="50"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+                </svg>
               </div>
-              <div className="icons__facebook"></div>
+              <div className="icons__facebook">
+                <img src={Facebook} alt="" />
+              </div>
             </div>
           </div>
           <div className="footer__container-subtext-flex">
