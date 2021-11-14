80c2a3f8d7892112c94b3d946a3dd485e1b92ec4
DocQuanty <Urchikak8@gmail.com>
2021-11-14 20:00:46 +0200

    create container for header add max-width 1000px, margin:auto

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,17 +4,19 @@ import Logo from "./Logo/Logo";
 
 const Header = () => {
   return (
-    <div className="header">
-      <Logo />
-      <div className="header__menu">
-        <div className="header__menu-games">
-          <NavLink to="/sectionGames">Games</NavLink>
-        </div>
-        <div className="header__menu-aboutUs">
-          <NavLink to="/sectionAboutUs">About Us</NavLink>
-        </div>
-        <div className="header__menu-career">
-          <NavLink to="/sectionCareer">Career</NavLink>
+    <div className="header-container">
+      <div className="header">
+        <Logo />
+        <div className="header__menu">
+          <div className="header__menu-games">
+            <NavLink to="/sectionGames">Games</NavLink>
+          </div>
+          <div className="header__menu-aboutUs">
+            <NavLink to="/sectionAboutUs">About Us</NavLink>
+          </div>
+          <div className="header__menu-career">
+            <NavLink to="/sectionCareer">Career</NavLink>
+          </div>
         </div>
       </div>
     </div>
