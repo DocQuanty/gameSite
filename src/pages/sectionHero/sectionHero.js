9023315fb3c043db6e422a714362346ef11e654c
DocQuanty <Urchikak8@gmail.com>
2021-11-17 14:17:36 +0200

    return

--- src/pages/sectionHero/sectionHero.js
+++ src/pages/sectionHero/sectionHero.js
@@ -4,7 +4,7 @@ import Header from "../../components/Header/Header";
 const SectionHero = (props) => {
   return (
     <div className="section-hero" onWheel={props.listenOnWheelEvent}>
-      <Header />
+      <Header paddingTop={20} marginBottom={560} />
       <div className="background"></div>
       <div className="container">
         <div className="section-title">
