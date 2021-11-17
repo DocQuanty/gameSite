c242503b696b481692a1e50d0fcebaeb72db6420
DocQuanty <Urchikak8@gmail.com>
2021-11-17 16:08:48 +0200

    change app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import "./App.scss";
+import { Switch, Route, Redirect } from "react-router";
 // import SectionHero from "./pages/sectionHero/sectionHero";
 import SectionAbout from "./pages/SectionAbout/SectionAbout";
+import Layout from "./components/hoc/Layout/Layout";
 
 const App = (props) => {
   return (
     <div className="App">
-      {/* <SectionHero /> */}
-      <SectionAbout />
+      <Layout>
+        <Switch>
+          <Route exact path="/sectionHero">
+            {/* <SectionHero /> */}
+          </Route>
+          <Route exact path="/sectionGames">
+            {/* <SectionGames /> */}
+          </Route>
+          <Route exact path="/sectionAbout">
+            <SectionAbout />
+          </Route>
+
+          <Route
+            render={() => (
+              <h1
+                style={{
+                  color: "red",
+                  margin: "150px 0px",
+                }}
+              >
+                404 not found
+                <p style={{ fontSize: "20px" }}>
+                  (or you are not allowed to enter the server page)
+                </p>
+              </h1>
+            )}
+          />
+          <Route render={() => <Redirect to={{ pathname: "/" }} />} />
+          {/* <Route exact path="/" component={SectionHero} /> */}
+        </Switch>
+      </Layout>
     </div>
   );
 };
